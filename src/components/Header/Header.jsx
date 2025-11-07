8e905502484950cad16346f85346cae7094a340b
nikoitservicedeutschland <nikoitservicedeutschland@gmail.com>
2025-11-07 13:34:46 +0000

    Add Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styles from './Header.module.css';
+import * as LucideIcons from 'lucide-react';
+
+function getIcon(name) {
+  const Icon = LucideIcons[name];
+  return Icon ? <Icon size={20} /> : null;
+}
+
+export const Header = ({ menuItems = [], className = '', ...props }) => (
+  <header className={styles.header_kui + ' ' + className} {...props}>
+    <nav className={styles.menu_kui}>
+      <ul className={styles.menuList_kui}>
+        {menuItems.map((item, idx) => (
+          <MenuItem key={idx} item={item} />
+        ))}
+      </ul>
+    </nav>
+  </header>
+);
+
+function MenuItem({ item }) {
+  const hasSubmenus = Array.isArray(item.submenus) && item.submenus.length > 0;
+  const isActive = !!item.active;
+  return (
+    <li className={
+      `${styles.menuItem_kui} ${isActive ? styles.active_kui : ''} ${hasSubmenus ? styles.hasSubmenu_kui : ''}`
+    }>
+      {item.link ? (
+        <a href={item.link} className={styles.menuLink_kui}>
+          {item.icon && getIcon(item.icon)}
+          <span>{item.label}</span>
+        </a>
+      ) : (
+        <span className={styles.menuLink_kui}>
+          {item.icon && getIcon(item.icon)}
+          <span>{item.label}</span>
+        </span>
+      )}
+      {hasSubmenus && (
+        <ul className={styles.submenuList_kui}>
+          {item.submenus.map((sub, idx) => (
+            <MenuItem key={idx} item={sub} />
+          ))}
+        </ul>
+      )}
+    </li>
+  );
+}
+
+Header.displayName = 'Header';import React from 'react';
+
+export const Header = ({ children, className = '', ...props }) => (
+  <header className={className} {...props}>
+    {children}
+  </header>
+);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 // Main entry point for @klarui/react
 export { Button } from './components/Button';
+export { Header } from './components/Header/Header';
+export { Header } from './components/Header';
