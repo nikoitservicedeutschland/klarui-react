6c8727dc6769fbe4a8906bf74152549dc983a9cf
nikoitservicedeutschland <nikoitservicedeutschland@gmail.com>
2025-11-11 10:41:18 +0000

    Add Layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 // Main entry point for @klarui/react
 export { Button } from './components/Button';
 export { Header } from './components/Header';
+export { Layout } from './components/Layout';
+export { Footer } from './components/Footer';
