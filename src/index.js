40c3b12bf7496bc8ef8bc3728d1d421d611e0bed
nikoitservicedeutschland <nikoitservicedeutschland@gmail.com>
2025-11-11 13:20:55 +0000

    Add Layout

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-// Export components (no global CSS import needed)
+// Import components
 import Button from './components/Button';
 import Header from './components/Header';
 import Layout from './components/Layout';
 import Footer from './components/Footer';
 
-// Default export (object with all components)
-export default {
+export { Button, Header, Layout, Footer };
+
+// Default export (در یک constant جداگانه)
+const KlarUI = {
   Button,
   Header,
   Layout,
-  Footer
+  Footer,
 };
 
-// Named exports
-export { Button, Header, Layout, Footer };
+export default KlarUI;
